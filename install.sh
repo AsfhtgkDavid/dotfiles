b6de55d87ea08d7f395a80434b68dc71a16de6d5
AsfhtgkDavid <asfhtgk.david@gmail.com>
2025-05-02 15:38:58 +0300

    Add plugin install

--- install.sh
+++ install.sh
@@ -21,9 +21,14 @@ if [ ! -d "$P10K_DIR" ]; then
   git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$P10K_DIR"
 fi
 
-echo "🔗 Linking kitty config..."
-mkdir -p "$HOME/.config/kitty"
-ln -sf "$DOTFILES_DIR/.config/kitty/kitty.conf" "$HOME/.config/kitty/kitty.conf"
+echo "⬇️ Installing zsh plugins..."
+
+ZSH_CUSTOM="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"
+
+git clone https://github.com/zsh-users/zsh-autosuggestions "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting "$ZSH_CUSTOM/plugins/fast-syntax-highlighting"
+git clone https://github.com/marlonrichert/zsh-autocomplete "$ZSH_CUSTOM/plugins/zsh-autocomplete"
 
 echo "🔗 Linking Neovim config..."
 mkdir -p "$HOME/.config/nvim"
